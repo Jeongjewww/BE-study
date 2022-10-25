127e8f72a7478f817c5a07e159b21d89fa315995
Jeongjewww <gwmelody99@gmail.com>
2022-10-26 01:10:06 +0900

    #9 unit testing

--- NestJS/nest-api/src/movies-api/movies-api.service.spec.ts
+++ NestJS/nest-api/src/movies-api/movies-api.service.spec.ts
@@ -1,4 +1,7 @@
+import { NotFoundException, ServiceUnavailableException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
+import { NotFoundError } from 'rxjs';
 import { MoviesApiService } from './movies-api.service';
 
 describe('MoviesApiService', () => {
@@ -15,4 +18,32 @@ describe('MoviesApiService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  })
+
+  describe("getOne", () => {
+    it("should return a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2000
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error", () => {
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  })
 });
