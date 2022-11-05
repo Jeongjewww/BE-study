b30b358499503a7e64334f438c504d7b1390a11b
Jeongjewww <gwmelody99@gmail.com>
2022-11-05 14:24:04 +0900

    #11 e2e testing

--- NestJS/nest-api/test/app.e2e-spec.ts
+++ NestJS/nest-api/test/app.e2e-spec.ts
@@ -1,24 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+    .get('/movies-api')
+    .expect(200)
+    .expect('[]');
+  });
+
+  describe("/movies-api", () => {
+    it("GET", () => {
+      return request(app.getHttpServer())
+      .get("/movies-api")
       .expect(200)
-      .expect('Hello World!');
+      .expect([]);
+    });
+    it("POST", () => {
+      return request(app.getHttpServer())
+        .post("/movies-api")
+        .send({
+          title: "Test",
+          year: 2020,
+          genres: ["test"]
+        })
+        .expect(201);
+    });
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+        .delete("/movies-api")
+        .expect(404);
+    });
+   });
+
+   describe("/movies-api/:id", () => {
+    it("GET 200", () => {
+      return request(app.getHttpServer())
+        .get("/movies-api/1")
+        .expect(200)
+    })
+    it("PATCH", () => {
+      return request(app.getHttpServer())
+        .patch("/movies-api/1")
+        .send({ year: 2022 })
+        .expect(200)
+    });
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+        .delete("/movies-api/1")
+        .expect(200)
+    });
   });
 });
