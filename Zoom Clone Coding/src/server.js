1be004f5e3d3917983c4e4a5fbb63d3dd1711a4e
Jeongjewww <gwmelody99@gmail.com>
2022-11-05 15:39:44 +0900

    ..#5 create 'room' event
    socketio의 emit method

--- Zoom Clone Coding/src/server.js
+++ Zoom Clone Coding/src/server.js
@@ -14,26 +14,13 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
 })
-
-// websocket code
-// const sockets = [];
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser");
-//     socket.on("close", () => { console.log("Disconnected from the Browser ❌"); });
-//     socket.on("message", (msg) => { 
-//         const message = JSON.parse(msg);
-//         switch (message.type) {
-//             case "new_message":
-//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//         }
-//     });
-// });
 
 const handleListen = () => console.log(`Listening on ws://localhost:3000/`)
 httpServer.listen(3000, handleListen);
