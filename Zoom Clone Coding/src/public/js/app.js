3cd3b6b33c8fa24d30b5a3737aee18cba6e5b746
Jeongjewww <gwmelody99@gmail.com>
2022-11-06 17:42:08 +0900

    #8 show room name

--- Zoom Clone Coding/src/public/js/app.js
+++ Zoom Clone Coding/src/public/js/app.js
@@ -4,18 +4,22 @@ const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
+let roomName;
 room.hidden = true;
 
 function enterRoom(){
     welcome.hidden = true;
     room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
     socket.emit("enter_room", input.value, enterRoom);
-    input.vaㅗlue = "";
+    roomName = input.value;
+    input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
